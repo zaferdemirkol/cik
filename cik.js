23d49c6a5de8381b0e456dc9d005e839d92d7e46
zaferdemirkol <zaferdemirkol@gmail.com>
2015-11-03 11:42:24 +0200

    ilk dosyalar

---
+++ cik.js
@@ -0,0 +1,440 @@
+﻿/*CiK (Çocuklar için Kodlama) çocukların kodlama (Programlama) kavramlarına kolayca alışabilmeleri için geliştirilmiş bir JavaScript Kütüphanesidir: http://www.cocuklaricinkodlama.com/cik
+CiK Zafer Demirkol - copyright (c) - http://www.zaferdemirkol.com Tarafından geliştirilmiş bir açık kaynak kod kütüphanesidir ve tüm telif hakları Zafer Demirkol'a aittir: http://www.zaferdemirkol.com/lisans
+Ticari olmayan amaçlar için serbestçe kullanılabilir. Buradaki kodlar kullanılarak herhangi bir ticari ürün veya hizmet geliştirilemez.
+Bu kodlara https://github.com/zaferdemirkol/cik adresinden erişebilir, gelişmeleri takip edebilir kütüphanenin gelişmesi için katkıda bulunabilirsiniz
+*/
+window.cik = (function () {
+
+    var c = document.getElementById("Canvas");
+    var ctx = c.getContext("2d");
+    var yukaridan;
+    var yandan;
+    var renk;
+    var genislik;
+    var yukseklik;
+    var ileriX;
+    var ileriY;
+
+
+    function CIK(els) {
+
+        for (var i = 0; i < els.length; i++) {
+            this[i] = els[i];
+        }
+        this.length = els.length;
+
+    }
+    i = 0,
+     length = 100;
+    function sagsol (z) {      
+      
+        ctx.lineTo(z, ileriY);              
+        ctx.stroke();
+       
+        
+    }
+
+    function yukariAsagi(t) {
+        // ctx.clearRect(0, 0, c.width, c.height);
+       
+        ctx.lineTo(ileriX, t);
+        ctx.stroke();
+    }
+
+    var cik = {
+
+        git: {
+            başlangıç: function (x, y) {
+                // ctx.clearRect(0, 0, c.width, c.height);
+                ileriX = x || 50;
+                ileriY = y || 50;
+
+                ctx.beginPath();
+                ctx.moveTo(x, ileriY);
+
+                return this;
+            },
+            sağ: function (z) {
+                ileriX += z;
+                sagsol(ileriX);
+
+                return this;
+            },
+
+            sol: function (z) {
+                ileriX -= z;
+                sagsol(ileriX);
+                return this;
+            },
+
+
+            aşağı: function (t) {
+                ileriY += t;
+                yukariAsagi(ileriY);
+                return this;
+
+            },
+
+            yukarı: function (t) {
+                ileriY -= t;
+                yukariAsagi(ileriY);
+                return this;
+
+            },
+            dön: function (aci) {
+                ctx.clearRect(0, 0, c.width, c.height);
+                ctx.rotate(aci * Math.PI / 180);
+                return this;
+            }
+
+
+        },
+
+        dikdörtgen: {
+            yatayUzaklık: 100,
+            dikeyUzaklık: 75,
+            genişlik: 150,
+            yükseklik:75,
+            renk: "#FF0000",
+
+            çiz: function () {
+                ctx.beginPath();               
+                ctx.fillRect(this.yatayUzaklık, this.dikeyUzaklık, this.genişlik, this.yükseklik);
+                ctx.fillStyle = this.renk;
+                ctx.fill();
+                ctx.stroke();
+            }
+        },
+
+        daire: {
+            yatayUzaklık: 100,
+            dikeyUzaklık: 75,
+            yarıçap: 50,
+            renk:"#FF0000",
+            çiz: function () {
+                ctx.beginPath();               
+                ctx.arc(this.yatayUzaklık, this.dikeyUzaklık, this.yarıçap, 0, 2 * Math.PI);
+                ctx.fillStyle = this.renk;
+                ctx.fill();
+                ctx.stroke();
+
+            }
+        },
+        
+        yıldız: {
+            yatayUzaklık: 75,
+            dikeyUzaklık: 100,
+            uçSayısı: 5,
+            outerRadius: 30,
+            innerRadius: 15,
+            renk: "orange",
+
+
+           çiz: function () {
+
+             
+               
+
+
+                var rot = Math.PI / 2 * 3;
+                var x = this.yatayUzaklık;
+                var y = this.dikeyUzaklık;
+                var step = Math.PI / this.uçSayısı;
+
+                ctx.strokeSyle = "#000";
+                ctx.beginPath();
+                ctx.moveTo(this.yatayUzaklık, this.dikeyUzaklık - this.outerRadius)
+                for (i = 0; i < this.uçSayısı; i++) {
+                    x = this.yatayUzaklık + Math.cos(rot) * this.outerRadius;
+                    y = this.dikeyUzaklık + Math.sin(rot) * this.outerRadius;
+                    ctx.lineTo(x, y)
+                    rot += step
+
+                    x = this.yatayUzaklık + Math.cos(rot) * this.innerRadius;
+                    y = this.dikeyUzaklık + Math.sin(rot) * this.innerRadius;
+                    ctx.lineTo(x, y)
+                    rot += step
+                }
+                ctx.lineTo(this.yatayUzaklık, this.dikeyUzaklık - this.outerRadius)
+                ctx.closePath();
+                ctx.lineWidth = 5;
+                ctx.strokeStyle = 'red';
+                ctx.stroke();
+                ctx.fillStyle = this.renk;
+                ctx.fill();
+
+
+
+
+            }
+        },
+        çokgen: {
+            yatayUzaklık: 150,
+            dikeyUzaklık: 100,
+            kenarSayısı: 5,
+            kenarUzunluğu: 100,
+            renk:"skyblue",
+
+            çiz:function () {
+
+                ctx.beginPath();
+                ctx.moveTo(this.yatayUzaklık + this.kenarUzunluğu * Math.cos(0), this.dikeyUzaklık + this.kenarUzunluğu * Math.sin(0));
+
+                for (var i = 1; i <= this.kenarSayısı; i += 1) {
+                    ctx.lineTo(this.yatayUzaklık + this.kenarUzunluğu * Math.cos(i * 2 * Math.PI / this.kenarSayısı), this.dikeyUzaklık + this.kenarUzunluğu * Math.sin(i * 2 * Math.PI / this.kenarSayısı));
+                }
+
+                ctx.strokeStyle = "#000000";
+
+                ctx.lineWidth = 3;
+                ctx.stroke();
+
+                ctx.fillStyle = this.renk;
+                ctx.fill();
+
+
+
+            }
+        },
+        tekrarla: function (uzunluk, icerik) {
+
+            
+            for (var i = 0; i < uzunluk; i++) {                
+               
+                eval(icerik)
+        
+            }
+
+      
+          
+
+        },
+        eğer: function (kosul, islem) {
+
+            if (eval(kosul)) {
+
+                eval(islem);
+
+            }
+
+
+
+        },
+        yaz: function (metin) {
+
+
+            var para = document.createElement("P");                       
+            var t = document.createTextNode(metin);           
+           
+            para.appendChild(t);                   
+
+            para.style.fontSize = "30px";
+            //    document.body.appendChild(para);
+            document.getElementById("icerik").appendChild(para)
+
+        },
+
+
+        resim: {
+            balık: function (yanX, ustY) {
+
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+         var   base_image = new Image();
+            base_image.src = 'images/fishu.png';
+            
+
+            base_image.onload = function () {
+                ctx.drawImage(base_image, yanX, ustY);
+            }
+            return this;
+            },
+       
+
+            git: function (yan, ust) {
+
+                
+                ctx.drawImage(base_image, yan, ust);
+                return this;
+
+            },
+
+            dön: function (aci) {
+              
+                ctx.translate(150, 200);
+           
+               
+                ctx.rotate(aci * Math.PI / 180);
+                ctx.translate(-150, -200);
+                return this;
+            },
+
+            köpek: function (yanX, ustY) {
+
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+                var base_image = new Image();
+                base_image.src = 'images/kopek.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+
+            kedi: function (yanX, ustY) {
+
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+                var base_image = new Image();
+                base_image.src = 'images/kedi.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+            penguen: function (yanX, ustY) {
+
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+                var base_image = new Image();
+                base_image.src = 'images/penguen.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+            gemi: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/gemi.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+            tramvay: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/tramvay.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+            karavan: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/karavan.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+
+            okulOtobüsü: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/okulotobusu.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+
+            elma: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/elma.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+
+            limon: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/limon.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            },
+
+            portakal: function (yanX, ustY) {
+                var yanX = yanX || 100;
+                var ustY = ustY || 100;
+
+
+                var base_image = new Image();
+                base_image.src = 'images/portakal.png';
+
+
+                base_image.onload = function () {
+                    ctx.drawImage(base_image, yanX, ustY);
+                }
+                return this;
+            }
+            
+        },
+       
+
+        yardım :  function () {
+       
+            window.open("yardim/cikYardim.html", "mywindow", "menubar=1,resizable=1,width=650,height=650");
+                        return this;
+                    }
+
+        
+
+
+
+
+    };
+
+
+
+
+
+    return cik;
+}());
---
+++ webIntellisense/kodTamamlama.js
@@ -0,0 +1,184 @@
+﻿function kodTamamlama() {
+
+
+
+
+    var editordenGelenMetin = satirIcerik.split(' ').join('');
+
+
+    function intellisenseOlustur() {
+        intellisense = new CodeMirrorIntellisense(editor);
+        intellisense.addDeclarationTrigger({ keyCode: 190 }); // `.`
+        intellisense.addMethodsTrigger({ keyCode: 57, shiftKey: true }); // `(`
+        intellisense.addMethodsTrigger({ keyCode: 48, shiftKey: true });// `)`
+        intellisense.addMethodsTrigger({ keyCode: 8 }); // `backspace`               
+
+    }
+
+    
+
+    switch (editordenGelenMetin) {
+        case "cik":
+
+
+            intellisenseOlustur();
+            intellisense.onDeclaration(function (item) {
+
+
+                var data = [
+                    { glyph: 3, name: 'yaz()', documentation: 'yaz', documentation: '("metin") Girilen metni yazar' },
+                    { glyph: 4, name: 'git', documentation: 'Bir çizim yapılacağını belirtir "başlangıç" metodu ile kullanılır' },
+                    { glyph: 4, name: 'dikdörtgen', documentation: 'Dikdörtgen çizer (yanUzaklık, yukarıUzaklık, genişlik, yükseklik, rengi) parametrelerini alır' },
+                    { glyph: 4, name: 'daire', documentation: 'Daire çizer (yanUzaklık, yukarıUzaklık, yarıçap,  rengi) parametrelerini alır' },
+                    { glyph: 4, name: 'yıldız', documentation: 'Yıldız çizer (yanUzaklık, yukarıUzaklık, YıldızUçSayısı) parametrelerini alır' },
+                    { glyph: 4, name: 'çokgen', documentation: 'Çokgen çizer (yanUzaklık, yukarıUzaklık, kenarSayısı, kenarUzunluğu) parametrelerini alır' },
+                    { glyph: 3, name: 'tekrarla()', documentation: 'Belirtilen sayıda tekrar yapar ("tekrar sayısı", "yapılacak iş")' },
+                     { glyph: 3, name: 'eğer()', documentation: 'Koşul (koşul ifadesi, "yapılacak iş")' },
+                     { glyph: 4, name: 'resim', documentation: 'Resim ekler' },
+                     { glyph: 3, name: 'yardım()', documentation: 'Yardım penceresi açar' }
+                ];
+                intellisense.setDeclarations(data);
+            });
+            break;
+
+
+        case "cik.daire":
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'çiz()', documentation: 'Daire çizer' },
+                    { glyph: 4, name: 'yarıçap', documentation: 'Yarıçap değeri' },
+                    { glyph: 4, name: 'yatayUzaklık', documentation: 'Daire merkezinin yatay uzaklığı' },
+                      { glyph: 4, name: 'dikeyUzaklık', documentation: 'Daire merkezinin dikey uzaklığı' },
+                        { glyph: 4, name: 'renk', documentation: 'Dairenin rengi' }
+                ];
+                intellisense.setDeclarations(data);
+
+
+            });
+            break;
+
+        case "cik.dikdörtgen":
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'çiz()', documentation: 'Dikdörtgen çizer' },
+                    { glyph: 4, name: 'genişlik', documentation: 'Dikdörtgenin genişliği' },
+                    { glyph: 4, name: 'yükseklik', documentation: 'Dikdörtgenin yüksekliği' },
+                    { glyph: 4, name: 'yatayUzaklık', documentation: 'Dikdörtgenin yatay uzaklığı' },
+                      { glyph: 4, name: 'dikeyUzaklık', documentation: 'Dikdörtgenin dikey uzaklığı' },
+                        { glyph: 4, name: 'renk', documentation: 'Dikdörtgenin rengi' }
+                ];
+                intellisense.setDeclarations(data);
+
+
+            });
+            break;
+
+        case "cik.yıldız":
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'çiz()', documentation: 'Yıldız çizer' },
+                    
+                    { glyph: 4, name: 'yatayUzaklık', documentation: 'Yıldızın yatay uzaklığı' },
+                      { glyph: 4, name: 'dikeyUzaklık', documentation: 'Yıldızın dikey uzaklığı' },
+                        { glyph: 4, name: 'uçSayısı', documentation: 'Yıldızın uç sayısı' },
+                        { glyph: 4, name: 'renk', documentation: 'Yıldızın rengi' }
+                ];
+                intellisense.setDeclarations(data);
+
+
+            });
+            break;
+        case "cik.çokgen":
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'çiz()', documentation: 'Çokgen çizer' },
+
+                    { glyph: 4, name: 'yatayUzaklık', documentation: 'Çokgenin yatay uzaklığı' },
+                      { glyph: 4, name: 'dikeyUzaklık', documentation: 'Çokgenin dikey uzaklığı' },
+                        { glyph: 4, name: 'kenarSayısı', documentation: 'Çokgenin kenar sayısı' },
+                         { glyph: 4, name: 'kenarUzunluğu', documentation: 'Çokgenin kenar uzunluğu' },
+                          { glyph: 4, name: 'renk', documentation: 'Çokgenin rengi' }
+                ];
+                intellisense.setDeclarations(data);
+
+
+            });
+            break;
+
+        case "cik.git":
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'başlangıç', documentation: 'İmleçi x,y koordinatında konumlandırır (x,y)' }
+                ];
+                intellisense.setDeclarations(data);
+
+
+            });
+            break;
+
+        case editordenGelenMetin.match(/(cik.git.başlangıç)?/)[1]:
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'sağ', documentation: 'Girilen değer kadar sağ yöne gider' },
+                    { glyph: 3, name: 'sol', documentation: 'Girilen değer kadar sol yöne gider' },
+                    { glyph: 3, name: 'aşağı', documentation:'Girilen değer kadar aşağı yöne gider' },
+                    { glyph: 3, name: 'yukarı', documentation:'Girilen değer yukarı aşağı yöne gider' }
+                ];
+                intellisense.setDeclarations(data);
+
+
+            });
+            break;
+
+        case "cik.resim":
+
+            intellisenseOlustur();
+
+            intellisense.onDeclaration(function (item) {
+                var data = [
+                    { glyph: 3, name: 'balık()', documentation: 'Balık resmi ekler' },
+                     { glyph: 3, name: 'köpek()', documentation: 'Köpek resmi ekler' },
+                     { glyph: 3, name: 'kedi()', documentation: 'Kedi resmi ekler' },
+                     { glyph: 3, name: 'penguen()', documentation: 'Penguen resmi ekler' },
+                     { glyph: 3, name: 'gemi()', documentation: 'Gemi resmi ekler' },
+                      { glyph: 3, name: 'tramvay()', documentation: 'Tramvay resmi ekler' },
+                      { glyph: 3, name: 'karavan()', documentation: 'Karavan resmi ekler' },
+                      { glyph: 3, name: 'okulOtobüsü()', documentation: 'Okul otobüsü resmi ekler' },
+                      { glyph: 3, name: 'elma()', documentation: 'Elma resmi ekler' },
+                       { glyph: 3, name: 'limon()', documentation: 'Limon resmi ekler' },
+                        { glyph: 3, name: 'portakal()', documentation: 'Portakal resmi ekler' }
+                ];
+                intellisense.setDeclarations(data);
+
+               
+            });
+            break;
+      
+
+        default:
+
+    }
+
+
+
+
+
+}
